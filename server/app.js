7db27ae72638e4e2a59b5b50cd1434e8d2b119ae
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2016-02-25 14:49:57 -0800

    compress html

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var swig = require('swig');
-var app = express();
-var path = require('path');
-var compress = require('compression');
 var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var minifyHTML = require('express-minify-html');
+var swig = require('swig');
+var compression = require('compression');
+
+const PORT = 3000;
+var app = express();
 
 app.engine('html', swig.renderFile);
 
@@ -18,12 +21,26 @@ if (env.production) {
   Object.assign(env, {
     assets: JSON.parse(fs.readFileSync(path.join(__dirname, '../assets.json')))
   });
+  app.use(compression());
+  app.use(minifyHTML({
+    override: true,
+    displayErrors: false,
+    htmlMinifier: {
+      removeComments: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      removeAttributeQuotes: true,
+      removeEmptyAttributes: true,
+      minifyJS: true
+    }
+  }));
 } else {
   app.set('view cache', false);
-  swig.setDefaults({ cache: false });
+  swig.setDefaults({
+    cache: false
+  });
 }
 
-app.use(compress());
 app.use('/static', express.static(path.join(__dirname, '../static/dist')));
 
 app.get('/', function (req, res) {
@@ -32,6 +49,9 @@ app.get('/', function (req, res) {
   });
 });
 
-app.listen(3005, function () {
-  console.log('Listening on port 3005!');
+app.listen(PORT, function (err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> 🌎 Listening on port %s. Open up http://127.0.0.1:%s/ in your browser.', PORT, PORT);
 });
