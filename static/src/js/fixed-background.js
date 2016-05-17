49ad025c47d1937a8a8e79fa3eb5c27dcbcd4373
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2016-05-17 10:35:57 -0700

    fix for ios

--- static/src/js/fixed-background.js
+++ static/src/js/fixed-background.js
@@ -1,22 +1,24 @@
 import $ from 'jquery';
 
-const hwRatio = 1080 / 1920;
-const $video = $('#home-video');
-const video = $video.get(0);
-resizeVideo();
-$video.removeClass('hide');
-video.play();
+if (!/iPad|iPhone|iPod/.test(navigator.platform)) {
+  const hwRatio = 1080 / 1920;
+  const $video = $('#home-video');
+  const video = $video.get(0);
+  resizeVideo();
+  $video.removeClass('hide');
+  video.play();
 
-function resizeVideo() {
-  const winHeight = $(window).height();
-  const winWidth = $(window).width();
-  if (winHeight / winWidth > hwRatio) {
-    video.setAttribute('height', winHeight);
-    video.removeAttribute('width');
-  } else {
-    video.setAttribute('width', winWidth);
-    video.removeAttribute('height');
+  function resizeVideo() {
+    const winHeight = $(window).height();
+    const winWidth = $(window).width();
+    if (winHeight / winWidth > hwRatio) {
+      video.setAttribute('height', winHeight);
+      video.removeAttribute('width');
+    } else {
+      video.setAttribute('width', winWidth);
+      video.removeAttribute('height');
+    }
   }
-}
 
-$(window).on('resize', resizeVideo);
+  $(window).on('resize', resizeVideo);
+}
