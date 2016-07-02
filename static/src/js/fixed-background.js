32f1c049d16b8d5a400c676dcf4590fc1ba86f1f
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2016-07-02 09:27:43 -0700

    fix jshint

--- static/src/js/fixed-background.js
+++ static/src/js/fixed-background.js
@@ -1,24 +1,27 @@
 import $ from 'jquery';
 
 if (!/iPad|iPhone|iPod/.test(navigator.platform)) {
-  const hwRatio = 1080 / 1920;
   const $video = $('#home-video');
   const video = $video.get(0);
-  resizeVideo();
+  resizeVideo(video);
   $video.removeClass('hide');
   video.play();
 
-  function resizeVideo() {
-    const winHeight = $(window).height();
-    const winWidth = $(window).width();
-    if (winHeight / winWidth > hwRatio) {
-      video.setAttribute('height', winHeight);
-      video.removeAttribute('width');
-    } else {
-      video.setAttribute('width', winWidth);
-      video.removeAttribute('height');
-    }
-  }
-
-  $(window).on('resize', resizeVideo);
+  $(window).on('resize', () => {
+    resizeVideo(video);
+  });
 }
+
+function resizeVideo(video) {
+  const hwRatio = 1080 / 1920;
+  const winHeight = $(window).height();
+  const winWidth = $(window).width();
+  if (winHeight / winWidth > hwRatio) {
+    video.setAttribute('height', winHeight);
+    video.removeAttribute('width');
+  } else {
+    video.setAttribute('width', winWidth);
+    video.removeAttribute('height');
+  }
+}
+
